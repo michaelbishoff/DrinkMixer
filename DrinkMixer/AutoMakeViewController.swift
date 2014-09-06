4583a2a6541db8573c1b9bbec418fb13303451b9
Robert Mozayeni <dev@mozayeni.com>
2014-09-06 06:17:57 -0400

    idek

--- DrinkMixer/AutoMakeViewController.swift
+++ DrinkMixer/AutoMakeViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class AutoMakeViewController: UIViewController {
+    @IBOutlet var timeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +23,15 @@ class AutoMakeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func makeIt() {
+        
     }
-    */
+    
+    @IBAction func makeItAlreadyPressed(sender: UIButton) {
+        
+    }
 
+    @IBAction func cancelPressed(sender: UIButton) {
+        
+    }
 }
---
+++ DrinkMixer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrinkMixer
+//
+//  Created by Robert S Mozayeni on 9/6/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
