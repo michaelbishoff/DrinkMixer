963434aa98977fac2d5662cb98e07a669c185819
Robert Mozayeni <dev@mozayeni.com>
2014-09-06 03:45:59 -0400

    Integrating AutoMakeVC

---
+++ DrinkMixer/AutoMakeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AutoMakeViewController.swift
+//  DrinkMixer
+//
+//  Created by Robert S Mozayeni on 9/6/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+import UIKit
+
+class AutoMakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DrinkMixer/DrinkHandler.swift
@@ -0,0 +1,27 @@
+
+//
+//  DrinkHandler.swift
+//  DrinkMixer
+//
+//  Created by Robert S Mozayeni on 9/6/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+import Foundation
+
+
+enum MixedDrinkType {
+    case OrangeTriangle
+    case Screwdriver
+    case CranberryKiss
+    case Hurricane
+    case HurricanePunch
+    case MarinaPunch
+    case MyPleasure
+}
+
+class DrinkHandler {
+    class func makeDrink(drinkType: MixedDrinkType){
+        //Make drink ¯\_(ツ)_/¯
+    }
+}
